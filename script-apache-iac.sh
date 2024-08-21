316fcfc0003ce5e3ae58ba84151b3839d41cc2b9
Angelo Gabriel Almeida Pereira <angelogabrieeel@gmail.com>
2024-08-21 18:14:06 -0300

    linux-iac

---
+++ script-apache-iac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Atualizando o servidor..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+cd /tmp
+#link do arquivo
+
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+#diretorio
+cd linux-site-dio-main
+cp -R * /var/www/html/
